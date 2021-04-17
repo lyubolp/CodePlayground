1b0864073459bc1b94b3bdc0213f434a1707624a
lyubolp <lyubo.karev@gmail.com>
2021-04-17 20:08:43 +0300

    Added platform struct, to keep platform info

---
+++ src/platform.rs
@@ -0,0 +1,49 @@
+pub mod platform {
+    use crate::config::config::get_platform_executable;
+
+    #[derive(Debug)]
+    pub struct Platform {
+        name: String,
+        executable: String,
+        file_extension: String
+    }
+
+    impl Platform {
+        pub fn new(name: &str, executable: &str, file_extension: &str) -> Self {
+            Platform {
+                name: String::from(name),
+                executable: String::from(executable),
+                file_extension: String::from(file_extension)
+            }
+        }
+
+        pub fn from_config(name: &str) -> Result<Self, String> {
+            let executable: String = match get_platform_executable(name) {
+                Ok(value) => value,
+                Err(value) => return Err(value)
+            };
+
+            let file_extension = match get_platform_executable(name) {
+                Ok(value) => value,
+                Err(value) => return Err(value)
+            };
+            Result::Ok(Platform {
+                name: String::from(name),
+                executable,
+                file_extension
+            })
+        }
+
+        pub fn get_name(&self) -> &str {
+            self.name.as_str()
+        }
+
+        pub fn get_executable(&self) -> &str {
+            self.executable.as_str()
+        }
+
+        pub fn get_file_extension(&self) -> &str {
+            self.file_extension.as_str()
+        }
+    }
+}
