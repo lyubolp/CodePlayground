74f27c3470c7aea74f4a2e58d1d3d4ce88dfefe9
Lyuboslav Karev <lyubo.karev@gmail.com>
2023-01-25 13:04:27 +0200

    Added docstrings to compiled language

--- src/compiled_language.py
+++ src/compiled_language.py
@@ -1,14 +1,28 @@
-from abc import ABC, abstractmethod
+"""
+Module containing the interface for compiled languages
+"""
+from abc import abstractmethod
 
 from src.language import Language
 from src.program_output import ProgramOutput
-from src.result import Ok, Error, Result
+from src.result import Result
+
 
 class CompiledLanguage(Language):
+    """
+    Interface for compiled languages
+    """
     @abstractmethod
     def compile(self, code_path: str) -> Result[ProgramOutput, str]:
-        pass
+        """
+        Compiles the code at the given path
+
+        Returns:
+            Result[ProgramOutput, str]: The result of the compilation
+        """
 
     @abstractmethod
     def get_executalbe_path(self) -> str:
-        pass
+        """
+        Returns the path to the executable
+        """
