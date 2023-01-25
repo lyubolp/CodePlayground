e85e175bf519d559d0b5843c92989bdc95ea9cb4
Lyuboslav Karev <lyubo.karev@gmail.com>
2023-01-25 13:26:59 +0200

    Added the ability to create a LanguageInformation from the Config

--- src/config.py
+++ src/config.py
@@ -4,7 +4,10 @@ This file contains the configuration class for the application.
 import json
 import os
 
-from typing import Any, Optional
+from typing import Any, Dict, Optional
+
+from src.language import LanguageInformation
+from src.result import Result, Ok, Error
 
 
 class Config:
@@ -25,3 +28,23 @@ class Config:
 
     def __getattribute__(self, __name: str) -> Any:
         return self.__config[__name]
+
+    def create_language_information(self, language: str) -> Result[LanguageInformation, str]:
+        """
+        Create a LanguageInformation object from the config.
+
+        :param language: The language to create the object for.
+        :return: The LanguageInformation object or an error.
+        """
+        if self.__config == {}:
+            return Error('Config is not loaded')
+
+        languages: Dict[str, dict] = self.__config['languages']
+
+        if language not in languages:
+            return Error('Language not found')
+
+        current_language: Dict[str, str] = languages[language]
+
+        return Ok(LanguageInformation(language, current_language['executable'],
+                                      current_language['extension'], current_language['compiled']))
