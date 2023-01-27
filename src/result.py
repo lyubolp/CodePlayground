a9e6868b2f57fe187d2589d939f51b28b89c480e
Lyuboslav Karev <lyubo.karev@gmail.com>
2023-01-27 20:31:26 +0200

    Moved away from value/error properties, in favor of a single value() method

--- src/result.py
+++ src/result.py
@@ -1,5 +1,5 @@
 """Result type for Python"""
-from abc import ABC
+from abc import ABC, abstractmethod
 from typing import Generic, TypeVar, Union
 
 T = TypeVar("T")
@@ -15,11 +15,23 @@ class BaseResult(ABC, Generic[T, E]):
     def __bool__(self) -> bool:
         return self._is_ok
 
+    @abstractmethod
+    def __eq__(self, other) -> bool:
+        """
+        Compare two results.
+        """
+
     def is_ok(self) -> bool:
         """
         Returns true if the result is Ok.
         """
         return self._is_ok
+
+    @abstractmethod
+    def value(self) -> Union[T, E]:
+        """
+        Get the value of the result.
+        """
 
 
 class Ok(BaseResult[T, E]):
@@ -34,6 +46,9 @@ class Ok(BaseResult[T, E]):
         """
         return self.__value
 
+    def __eq__(self, other) -> bool:
+        return isinstance(other, Ok) and self.__value == other.value()
+
 
 class Error(BaseResult[T, E]):
     """Error result type"""
@@ -41,11 +56,14 @@ class Error(BaseResult[T, E]):
         super().__init__(False)
         self.__error: E = error
 
-    def error(self) -> E:
+    def value(self) -> E:
         """
         Get the error of the result.
         """
         return self.__error
+
+    def __eq__(self, other) -> bool:
+        return isinstance(other, Error) and self.__error == other.value()
 
 
 Result = Union[Ok[T, E], Error[T, E]]
