ba521a4f88433a6839ea0dd40811d6a0828373ae
Lyuboslav Karev <lyubo.karev@gmail.com>
2023-01-27 20:26:53 +0200

    Split the LanguageInformation class into a new module

--- src/language.py
+++ src/language.py
@@ -4,10 +4,10 @@ Module containing all the abstractions around languages.
 import subprocess
 
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
 from typing import List
 
 from src.config import Config
+from src.language_information import LanguageInformation
 from src.program_output import ProgramOutput
 from src.result import Ok, Error, Result
 
@@ -26,18 +26,6 @@ def execute_command(command: str, args: List[str]) -> Result:
         return Error(exception.stderr)
 
     return Ok(result.stdout)
-
-
-@dataclass
-class LanguageInformation:
-    """
-    Information about a language.
-    """
-    name: str
-    executable: str
-    file_extension: str
-    is_compiled: str
-
 
 class Language(ABC):
     """
