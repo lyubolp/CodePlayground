2cb766bcc3528c853eebb2b3997b2d7e1a7a0805
Lyuboslav Karev <lyubo.karev@gmail.com>
2023-01-30 22:23:17 +0200

    Added a simple main file

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 """
 Main program code
 """
+
+from src.config import Config
+from src.executor import Executor
+from src.languages.python import Python
+
 if __name__ == "__main__":
-    print("Hello world")
+    config = Config('config.json')
+
+    python_executor = Executor(Python(config))
+    python_res = python_executor.run('print(2+3)')
+
+    print(python_res)
