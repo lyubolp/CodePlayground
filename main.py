3d1efbd35db63d81b4431eb46623fe3c44125470
Lyuboslav Karev <lyubo.karev@gmail.com>
2023-01-30 22:53:37 +0200

    Added C++ test code to main

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ Main program code
 
 from src.config import Config
 from src.executor import Executor
+from src.languages.cpp import Cpp
 from src.languages.python import Python
 
 if __name__ == "__main__":
@@ -13,3 +14,8 @@ if __name__ == "__main__":
     python_res = python_executor.run('print(2+3)')
 
     print(python_res)
+
+    cpp_executor = Executor(Cpp(config))
+    cpp_res = cpp_executor.run('#include <iostream>\nint main()\n{\n\tstd::cout << 2+3;\n\treturn 0;\n}')
+
+    print(cpp_res)
